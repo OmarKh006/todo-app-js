8cbce6d3f3dc00d6c117b6f46d817603e71431d4
OmarKh006 <khairyomar006@gmail.com>
2026-04-15 14:16:21 +0200

    add task ✅

--- index.js
+++ index.js
@@ -1,5 +1,45 @@
-import { darkThemeButton, mainApp } from "./scripts/elements";
+import {
+  darkThemeButton,
+  inputField,
+  mainApp,
+  submitTaskButton,
+  taskList,
+} from "./scripts/elements";
+
+const addTask = (e) => {
+  e.preventDefault();
+
+  const taskValue = inputField.value;
+
+  if (!taskValue) return;
+
+  taskList.innerHTML = `
+  <li class="TaskList__taskContent">
+    <div class="TaskList__checkbox" tabindex="0" role="button">
+        <img
+            src="./assets/icon-checkmark.svg"
+            alt="check icon"
+            class="TaskList__checkboxImg"
+        />
+    </div>
+    <div class="TaskList__valueContent">
+    <p class="TaskList__value">
+        ${taskValue}
+    </p>
+    <img
+        class="TaskList__deleteIcon"
+        src="./assets/icon-basket.svg"
+        alt="basket icon"
+    />
+    </div>
+    </li>
+  `;
+};
 
 darkThemeButton?.addEventListener("click", () => {
   mainApp?.classList.toggle("App--isDark");
+});
+
+submitTaskButton.addEventListener("click", (e) => {
+  addTask(e);
 });
--- scripts/elements.js
+++ scripts/elements.js
@@ -1,2 +1,7 @@
 export const darkThemeButton = document.querySelector(".DarkThemeToggle");
 export const mainApp = document.querySelector(".App");
+export const inputField = document.querySelector(".TaskSearchBar__input");
+export const taskList = document.querySelector(".TaskList__list");
+export const submitTaskButton = document.querySelector(
+  ".TaskSearchBar__button",
+);
