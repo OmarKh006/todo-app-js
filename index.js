bcbc3ba04b9d1c79d6e3937bb9af0d5d02f320f7
OmarKh006 <khairyomar006@gmail.com>
2026-04-16 07:58:52 +0200

    sorting by drag and drop

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
+import Sortable from "sortablejs";
 import { initDataOnStartup } from "./scripts/initDataOnStartup";
+import { taskList } from "./scripts/elements";
 
 initDataOnStartup();
+
+new Sortable(taskList, {
+  animation: 150,
+});
